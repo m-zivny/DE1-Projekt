acc4d137a771544dd7b38570c7e88d5908f5e16c
Živný Martin <145465801+m-zivny@users.noreply.github.com>
2025-05-08 16:51:15 +0200

    finalni upload tb souboru

---
+++ tb_source/tb_top_level.vhd
@@ -0,0 +1,120 @@
+-- Testbench automatically generated online
+-- at https://vhdl.lapinoo.net
+-- Generation date : Thu, 01 May 2025 19:18:04 GMT
+-- Request id : cfwk-fed377c2-6813c8ec643ac
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_top_level is
+end tb_top_level;
+
+architecture tb of tb_top_level is
+
+    component top_level
+        port (CLK100MHZ   : in std_logic;
+              US1_TRIGGER : out std_logic;
+              US2_TRIGGER : out std_logic;
+              US1_ECHO    : in std_logic;
+              US2_ECHO    : in std_logic;
+              AN          : out std_logic_vector (7 downto 0);
+              CA          : out std_logic;
+              CB          : out std_logic;
+              CC          : out std_logic;
+              CD          : out std_logic;
+              CE          : out std_logic;
+              CF          : out std_logic;
+              CG          : out std_logic;
+              DP          : out std_logic;
+              LED16_G     : out std_logic;
+              LED16_R     : out std_logic;
+              LED17_G     : out std_logic;
+              LED17_R     : out std_logic);
+    end component;
+
+    signal CLK100MHZ   : std_logic;
+    signal US1_TRIGGER : std_logic;
+    signal US2_TRIGGER : std_logic;
+    signal US1_ECHO    : std_logic;
+    signal US2_ECHO    : std_logic;
+    signal AN          : std_logic_vector (7 downto 0);
+    signal CA          : std_logic;
+    signal CB          : std_logic;
+    signal CC          : std_logic;
+    signal CD          : std_logic;
+    signal CE          : std_logic;
+    signal CF          : std_logic;
+    signal CG          : std_logic;
+    signal DP          : std_logic;
+    signal LED16_G     : std_logic;
+    signal LED16_R     : std_logic;
+    signal LED17_G     : std_logic;
+    signal LED17_R     : std_logic;
+    constant TbPeriod : time := 10 ns; -- ***EDIT*** Put right period here
+    signal TbClock : std_logic := '0';
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    dut : top_level
+    port map (CLK100MHZ   => CLK100MHZ,
+              US1_TRIGGER => US1_TRIGGER,
+              US2_TRIGGER => US2_TRIGGER,
+              US1_ECHO    => US1_ECHO,
+              US2_ECHO    => US2_ECHO,
+              AN          => AN,
+              CA          => CA,
+              CB          => CB,
+              CC          => CC,
+              CD          => CD,
+              CE          => CE,
+              CF          => CF,
+              CG          => CG,
+              DP          => DP,
+              LED16_G     => LED16_G,
+              LED16_R     => LED16_R,
+              LED17_G     => LED17_G,
+              LED17_R     => LED17_R);
+
+    -- Clock generation
+    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    -- ***EDIT*** Check that CLK100MHZ is really your main clock signal
+    CLK100MHZ <= TbClock;
+
+    stimuli : process
+        variable us1_echo_pulse : time := 5 us;
+        variable us2_echo_pulse : time := 13 us;
+        variable period : time := 100 us;
+    begin
+        US1_ECHO <= '0';
+        US2_ECHO <= '0';
+        wait for 100025 ns;
+        for i in 1 to 160 loop
+        US1_ECHO <= '1';
+        US2_ECHO <= '1';
+        wait for us1_echo_pulse;
+        US1_ECHO <= '0';    
+        wait for (us2_echo_pulse-us1_echo_pulse);
+        US2_ECHO <= '0';
+        wait for (period - us2_echo_pulse + 10 ns); 
+        if ((us1_echo_pulse < 40 us)and (us2_echo_pulse < 40 us)) then
+            us1_echo_pulse := us1_echo_pulse + 1 us;
+            us2_echo_pulse := us2_echo_pulse + 1 us;
+        else
+            us1_echo_pulse := 5 us;
+            us2_echo_pulse := 13 us;
+        end if;
+        end loop;
+        TbSimEnded <= '1';
+        wait;
+    end process;
+
+end tb;
+
+-- Configuration block below is required by some simulators. Usually no need to edit.
+
+configuration cfg_tb_top_level of tb_top_level is
+    for tb
+    end for;
+end cfg_tb_top_level;
